caedd98b60917f83a6e0df8f7c3254da6bccf785
Saverio Morelli <saverio.morelli@outlook.it>
2021-09-20 19:20:10 +0200

    Update definitions.js

--- js/definitions.js
+++ js/definitions.js
@@ -6,3 +6,5 @@ let supportedLanguages = ["en", "it"];
 let languageToUse = browser.i18n.getUILanguage().toString();
 
 if (!supportedLanguages.includes(languageToUse)) languageToUse = "en";
+
+if (supportedLanguages.includes(languageToUse.split("-")[0])) languageToUse = languageToUse.split("-")[0];
