b9f678c53b53087631be4b9730fce212fdf7f5ff
Saverio Morelli <saverio.morelli@outlook.it>
2021-10-29 19:04:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var selected_tab = 0; //{0:domain | 1:page}
 const all_strings = strings[languageToUse];
 
 const linkReview = ["https://addons.mozilla.org/firefox/addon/websites-notes/"]; //{firefox add-ons}
-const linkDonate = ["https://www.paypal.com/pools/c/8yl6auiU6e", "https://ko-fi.com/saveriomorelli", "https://liberapay.com/Sav22999/donate"]; //{paypal, ko-fi}
+const linkDonate = ["https://www.paypal.me/saveriomorelli", "https://ko-fi.com/saveriomorelli", "https://liberapay.com/Sav22999/donate"]; //{paypal, ko-fi}
 
 function loaded() {
     document.addEventListener("contextmenu",
