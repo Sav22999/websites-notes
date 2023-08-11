3c5c4c957eadaf227af73e37a0f5d7ebee8f336f
Sav22999 <saverio.morelli@outlook.it>
2023-08-12 01:29:22 +0200

    languages

--- js/definitions.js
+++ js/definitions.js
@@ -2,7 +2,7 @@ var lang = "";
 
 var strings = []; //strings[language_code] = {};
 
-let supportedLanguages = ["en", "it", "ar", "zh-CN", "zh-TW", "cs", "da", "nl", "fi", "fr", "de", "el", "ja", "no", "pl", "pt", "pt-BR", "ro", "ru", "es", "sv-SE", "uk"];
+let supportedLanguages = ["en", "it", "ar", "zh-cn", "zh-tw", "cs", "da", "nl", "fi", "fr", "de", "el", "ja", "pl", "pt-pt", "pt-br", "ro", "ru", "es", "sv-SE", "uk"];
 let languageToUse = browser.i18n.getUILanguage().toString();
 
 if (!supportedLanguages.includes(languageToUse)) languageToUse = "en";
