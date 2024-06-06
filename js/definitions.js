a3bd9878fceac6c43c5e295006e903311821ff23
smorelli <saverio.morelli@outlook.it>
2024-06-06 15:53:59 +0200

    Fixed a bug in screenshots and in languages supported

--- js/definitions.js
+++ js/definitions.js
@@ -2,11 +2,11 @@ var lang = "";
 
 var strings = []; //strings[language_code] = {};
 
-let supportedLanguages = ["en", "it", "ar", "zh-cn", "zh-tw", "cs", "da", "nl", "fi", "fr", "de", "el", "ja", "pl", "pt-pt", "pt-br", "ro", "ru", "es", "sv-SE", "uk"];
+//TODO!manually: add new languages here
+let supportedLanguages = ["en", "it", "ar", "zh-cn", "zh-tw", "cs", "da", "nl", "fi", "fr", "de", "el", "ja", "pl", "pt-pt", "pt-br", "ro", "ru", "es", "sv-SE", "uk", "ia"];
+
 let languageToUse = browser.i18n.getUILanguage().toString();
-
 if (!supportedLanguages.includes(languageToUse)) languageToUse = "en";
-
 if (supportedLanguages.includes(languageToUse.split("-")[0])) languageToUse = languageToUse.split("-")[0];
 
 let links = {
